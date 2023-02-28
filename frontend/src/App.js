bbd2ad87ca32816d37da000abc063bb840c73862
Артем Черных <acac1212@yandex.ru>
2023-02-28 14:28:24 +1000

    add common containers to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,21 @@ import { NotFoundPage } from './components/NotFoundPage.jsx';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<MainPage />} />
-        <Route path='login' element={<LoginPage />} />
-        <Route path='*' element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="h-100">
+      <div className="h-100" id="chat">
+        <div className="d-flex flex-column h-100">
+          <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white"><div className="container"><a className="navbar-brand" href="/">Hexlet Chat</a></div></nav>
+          <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<MainPage />} />
+              <Route path='login' element={<LoginPage />} />
+              <Route path='*' element={<NotFoundPage />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </div>
+    </div>
+    
   );
 }
 
