b2821f3f4e13b5edd4b9f664fe1f54e27c421bd4
Артем Черных <acac1212@yandex.ru>
2023-03-14 13:40:51 +1000

    add focus on message form

--- frontend/src/components/MainPage/Messages.jsx
+++ frontend/src/components/MainPage/Messages.jsx
@@ -1,13 +1,18 @@
 import { useSelector } from "react-redux";
 import { Col, Form, Button } from 'react-bootstrap';
 import { useFormik } from 'formik';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { useSocket } from "../providers/SocketProvider.jsx";
 import { messagesSelectors } from "../../slices/messagesSlice.js";
 
 const MessageForm = ({ activeChannel }) => {
   const [error, setError] = useState(null);
+  const inputRef = useRef();
   const { sendMessage } = useSocket();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
 
   const formik = useFormik({
     initialValues: {
@@ -34,7 +39,7 @@ const MessageForm = ({ activeChannel }) => {
   return (
     <Form novalidate="" className="py-1 border rounded-2" onSubmit={formik.handleSubmit}>
       <Form.Group className="has-validation input-group">
-        <Form.Control name="body" aria-label="Новое сообщение" placeholder="Введите сообщение..." className="border-0 p-0 ps-2" value={formik.values.body} onChange={formik.handleChange} />
+        <Form.Control name="body" aria-label="Новое сообщение" placeholder="Введите сообщение..." className="border-0 p-0 ps-2" ref={inputRef} value={formik.values.body} onChange={formik.handleChange} />
         <Button variant="group-vertical" type="submit" disabled={formik.isSubmitting}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
             <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
