cb9ac88311bed9739d78fd11bdf82c41d1626641
Артем Черных <acac1212@yandex.ru>
2023-03-20 12:40:16 +1000

    add Rollbar

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -2,6 +2,7 @@ import { Provider } from 'react-redux';
 import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 import filter from 'leo-profanity';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import store from './slices/index.js';
 import AuthProvider from './components/providers/AuthProvider.jsx';
 import SocketProvider from './components/providers/SocketProvider.jsx';
@@ -22,16 +23,25 @@ const init = async (socket) => {
   filter.add(filter.getDictionary('ru'));
   filter.add(filter.getDictionary('en'));
 
+  const rollbarConfig = {
+    accessToken: '39da1795bb614b51b7bcdb6ba2e247ee',
+    environment: 'production',
+  };
+
   return (
-    <I18nextProvider i18n={i18n}>
-      <Provider store={store}>
-        <AuthProvider>
-          <SocketProvider socket={socket}>
-            <App />
-          </SocketProvider>
-        </AuthProvider>
-      </Provider>
-    </I18nextProvider>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <I18nextProvider i18n={i18n}>
+          <Provider store={store}>
+            <AuthProvider>
+              <SocketProvider socket={socket}>
+                <App />
+              </SocketProvider>
+            </AuthProvider>
+          </Provider>
+        </I18nextProvider>
+      </ErrorBoundary>
+    </RollbarProvider>
   );
 };
 
