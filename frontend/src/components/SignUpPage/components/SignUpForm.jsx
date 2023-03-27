6fdf8ccd046dee4d8ca60017fe7d90bf3703afcf
Артем Черных <acac1212@yandex.ru>
2023-03-27 18:16:55 +1000

    remove checkErrors from SignUpForm

--- frontend/src/components/SignUpPage/components/SignUpForm.jsx
+++ frontend/src/components/SignUpPage/components/SignUpForm.jsx
@@ -1,7 +1,5 @@
-/* eslint-disable no-param-reassign */
 import { Form, Button } from 'react-bootstrap';
-import { useRef, useEffect } from 'react';
-import { useImmer } from 'use-immer';
+import { useRef, useEffect, useState } from 'react';
 import { Formik } from 'formik';
 import * as yup from 'yup';
 import axios from 'axios';
@@ -11,49 +9,9 @@ import { toast } from 'react-toastify';
 import { useAuth } from '../../providers/AuthProvider';
 import routes from '../../../routes';
 
-const checkErrors = (errors, isSubmitting, updateSignUpErrors) => {
-  console.log('errors', errors);
-  if (isSubmitting) {
-    if (errors.username) {
-      updateSignUpErrors((state) => {
-        state.username = errors.username;
-      });
-    } else {
-      updateSignUpErrors((state) => {
-        state.username = null;
-      });
-    }
-
-    if (errors.password) {
-      updateSignUpErrors((state) => {
-        state.password = errors.password;
-      });
-    } else {
-      updateSignUpErrors((state) => {
-        state.password = null;
-      });
-    }
-
-    if (errors.confirmPassword) {
-      updateSignUpErrors((state) => {
-        state.confirmPassword = errors.confirmPassword;
-      });
-    } else {
-      updateSignUpErrors((state) => {
-        state.confirmPassword = null;
-      });
-    }
-  }
-};
-
 const SignUpForm = () => {
   const { t } = useTranslation();
-  const [signUpErrors, updateSignUpErrors] = useImmer({
-    username: null,
-    password: null,
-    confirmPassword: null,
-    alreadyExist: null,
-  });
+  const [signUpFailed, setSignUpFailed] = useState(false);
   const inputRef = useRef();
   const auth = useAuth();
   const navigate = useNavigate();
@@ -80,9 +38,7 @@ const SignUpForm = () => {
       }}
       validationSchema={validationSchema}
       onSubmit={async (values) => {
-        updateSignUpErrors((state) => {
-          state.alreadyExist = null;
-        });
+        setSignUpFailed(false);
 
         try {
           const res = await axios.post(routes.apiSignUp(), values);
@@ -96,10 +52,7 @@ const SignUpForm = () => {
             }
 
             if (error.response.status === 409) {
-              updateSignUpErrors((state) => {
-                state.alreadyExist = 'alreadyExist';
-              });
-
+              setSignUpFailed(true);
               inputRef.current.select();
               return;
             }
@@ -110,92 +63,88 @@ const SignUpForm = () => {
       }}
     >
       {({
-        handleSubmit, handleChange, values, isSubmitting, errors,
-      }) => {
-        checkErrors(errors, isSubmitting, updateSignUpErrors);
-
-        return (
-          <Form
-            className="col-12 col-md-6 mt-3 mt-mb-0"
-            onSubmit={handleSubmit}
+        handleSubmit, handleChange, values, isSubmitting, errors, touched,
+      }) => (
+        <Form
+          className="col-12 col-md-6 mt-3 mt-mb-0"
+          onSubmit={handleSubmit}
+        >
+          <h1 className="text-center mb-4">{t('signUpPage.registration')}</h1>
+          <Form.Group className="form-floating mb-3">
+            <Form.Control
+              name="username"
+              autoComplete="username"
+              placeholder={t('signUpPage.incorrectUsernameLength')}
+              id="username"
+              type="login"
+              isInvalid={
+                (errors.username && touched.username) || signUpFailed
+              }
+              ref={inputRef}
+              onChange={handleChange}
+              value={values.username}
+            />
+            <Form.Label htmlFor="username">
+              {t('signUpPage.username')}
+            </Form.Label>
+            <Form.Control.Feedback type="invalid">
+              {errors.username && t(`errors.${errors.username}`)}
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group className="form-floating mb-3">
+            <Form.Control
+              name="password"
+              autoComplete="new-password"
+              placeholder={t('signUpPage.incorrectPasswordLength')}
+              id="password"
+              type="password"
+              aria-describedby="passwordHelpBlock"
+              isInvalid={
+                (errors.password && touched.password) || signUpFailed
+              }
+              onChange={handleChange}
+              value={values.password}
+            />
+            <Form.Label htmlFor="password">
+              {t('signUpPage.password')}
+            </Form.Label>
+            <Form.Control.Feedback type="invalid">
+              {errors.password && t(`errors.${errors.password}`)}
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group className="form-floating mb-4">
+            <Form.Control
+              name="confirmPassword"
+              autoComplete="new-password"
+              placeholder={t('signUpPage.shouldConfirm')}
+              id="confirmPassword"
+              type="password"
+              isInvalid={
+                (errors.confirmPassword && touched.confirmPassword) || signUpFailed
+              }
+              onChange={handleChange}
+              value={values.confirmPassword}
+            />
+            <Form.Label htmlFor="confirmPassword">
+              {t('signUpPage.confirmPassword')}
+            </Form.Label>
+            <Form.Control.Feedback type="invalid">
+              {errors.confirmPassword
+                ? t(`errors.${errors.confirmPassword}`)
+                : t('errors.alreadyExist')}
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Button
+            type="submit"
+            value="submit"
+            variant="outline-primary"
+            className="w-100"
+            disabled={isSubmitting || (Object.values(errors).length > 0 && Object.values(touched).length > 0)}
           >
-            <h1 className="text-center mb-4">{t('signUpPage.registration')}</h1>
-            <Form.Group className="form-floating mb-3">
-              <Form.Control
-                name="username"
-                autoComplete="username"
-                placeholder={t('signUpPage.incorrectUsernameLength')}
-                id="username"
-                type="login"
-                isInvalid={
-                  !!signUpErrors.username || !!signUpErrors.alreadyExist
-                }
-                ref={inputRef}
-                onChange={handleChange}
-                value={values.username}
-              />
-              <Form.Label htmlFor="username">
-                {t('signUpPage.username')}
-              </Form.Label>
-              <Form.Control.Feedback type="invalid">
-                {signUpErrors.username && t(`errors.${signUpErrors.username}`)}
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group className="form-floating mb-3">
-              <Form.Control
-                name="password"
-                autoComplete="new-password"
-                placeholder={t('signUpPage.incorrectPasswordLength')}
-                id="password"
-                type="password"
-                aria-describedby="passwordHelpBlock"
-                isInvalid={
-                  !!signUpErrors.password || !!signUpErrors.alreadyExist
-                }
-                onChange={handleChange}
-                value={values.password}
-              />
-              <Form.Label htmlFor="password">
-                {t('signUpPage.password')}
-              </Form.Label>
-              <Form.Control.Feedback type="invalid">
-                {signUpErrors.password && t(`errors.${signUpErrors.password}`)}
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group className="form-floating mb-4">
-              <Form.Control
-                name="confirmPassword"
-                autoComplete="new-password"
-                placeholder={t('signUpPage.shouldConfirm')}
-                id="confirmPassword"
-                type="password"
-                isInvalid={
-                  !!signUpErrors.confirmPassword || !!signUpErrors.alreadyExist
-                }
-                onChange={handleChange}
-                value={values.confirmPassword}
-              />
-              <Form.Label htmlFor="confirmPassword">
-                {t('signUpPage.confirmPassword')}
-              </Form.Label>
-              <Form.Control.Feedback type="invalid">
-                {signUpErrors.confirmPassword
-                  ? t(`errors.${signUpErrors.confirmPassword}`)
-                  : t(`errors.${signUpErrors.alreadyExist}`)}
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Button
-              type="submit"
-              value="submit"
-              variant="outline-primary"
-              className="w-100"
-              disabled={isSubmitting}
-            >
-              {t('signUpPage.register')}
-            </Button>
-          </Form>
-        );
-      }}
+            {t('signUpPage.register')}
+          </Button>
+        </Form>
+      )}
     </Formik>
   );
 };
