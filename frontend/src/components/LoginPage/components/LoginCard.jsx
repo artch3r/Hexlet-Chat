388f778d999f4c136693c7f38709171256103697
Артем Черных <acac1212@yandex.ru>
2023-04-19 08:35:19 +1000

    remove comments from auth cards and forms

--- frontend/src/components/LoginPage/components/LoginCard.jsx
+++ frontend/src/components/LoginPage/components/LoginCard.jsx
@@ -18,31 +18,3 @@ const LoginCard = ({ children }) => {
 };
 
 export default LoginCard;
-
-// const LoginCard = ({ children }) => {
-//   const { t } = useTranslation();
-
-//   return (
-//     <Card className="shadow-sm">
-//       <Card.Body className="row p-5">
-//         <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
-//           <img
-//             src={loginImage}
-//             className="rounded-circle"
-//             alt={t('loginPage.enter')}
-//           />
-//         </div>
-//         {children}
-//       </Card.Body>
-//       <Card.Footer className="p-4">
-//         <div className="text-center">
-//           <span>
-//             {t('loginPage.noAccount')}
-//             {' '}
-//           </span>
-//           <a href={pageRoutes.signUpPage()}>{t('loginPage.registration')}</a>
-//         </div>
-//       </Card.Footer>
-//     </Card>
-//   );
-// };
--- frontend/src/components/SignUpPage/components/SignUpCard.jsx
+++ frontend/src/components/SignUpPage/components/SignUpCard.jsx
@@ -18,32 +18,3 @@ const SignUpCard = ({ children }) => {
 };
 
 export default SignUpCard;
-
-// const SignUpCard = ({ children }) => {
-//   const { t } = useTranslation();
-
-//   return (
-//     <Card className="shadow-sm">
-//       <Card.Body
-// className="d-flex flex-column flex-md-row justify-content-around align-items-center p-5">
-//         <div>
-//           <img
-//             src={signUpImage}
-//             className="rounded-circle"
-//             alt={t('signUpPage.registration')}
-//           />
-//         </div>
-//         {children}
-//       </Card.Body>
-//       <Card.Footer className="p-4">
-//         <div className="text-center">
-//           <span>
-//             {t('signUpPage.alreadyHave')}
-//             {' '}
-//           </span>
-//           <a href={pageRoutes.loginPage()}>{t('signUpPage.login')}</a>
-//         </div>
-//       </Card.Footer>
-//     </Card>
-//   );
-// };
